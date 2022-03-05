3befcc7988a064c7a80940a88b0679ad2c846127
Barouali-AB <62695088+Barouali-AB@users.noreply.github.com>
2022-03-05 19:18:40 +0100

    Update scraping_data.py

--- scraping_data.py
+++ scraping_data.py
@@ -4,13 +4,22 @@ import csv
 import re
 
 
-data = open(r'hotel_reviews.csv', 'w', encoding="utf-8", newline="")
-writer = csv.writer(data)
+
 header = ["Review", "Rating"]
+
+
+data = open(r'hotel_reviews.csv', 'w', encoding="utf-8", newline="")
+
+
+writer = csv.writer(data)
+
 writer.writerow(header)
 
 headers = {'User-Agent':'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.97 Safari/537.36'}
 
+
+rating_counter = [0,0,0,0,0]
+full = False
 
 def scrap_data(url):
     page = requests.get(url, headers=headers)
@@ -21,10 +30,26 @@ def scrap_data(url):
 
 
     for data_review in list_data_reviews:
-        review = data_review.find('q', {'class': 'XllAv H4 _a'})
-        rating = data_review.find('div', {'class': 'emWez F1'})
-        row = [review.find('span').text.strip(), rating.find('span').attrs['class'][1][7]]
-        writer.writerow(row)
+
+        if ( rating_counter[0] == 5 and rating_counter[1] == 5 and rating_counter[2] == 5 and rating_counter[3] == 5 and
+        rating_counter[4] == 5 ):
+            full = True
+            break
+
+        else:
+            review = data_review.find('q', {'class': 'XllAv H4 _a'})
+            rating = data_review.find('div', {'class': 'emWez F1'})
+            rating_value = rating.find('span').attrs['class'][1][7]
+            if ( rating_counter[int(rating_value)-1] < 5 ):
+                row = [review.find('span').text.strip(), rating_value]
+                rating_counter[int(rating_value) - 1] += 1
+                writer.writerow(row)
+
+                print(rating_counter)
+            else:
+                continue
+
+
 
 
 def number_reviews(url):
@@ -34,7 +59,6 @@ def number_reviews(url):
 
     list_data_reviews = soup2.findAll('div', {'class': 'cqoFv _T'})
     return len(list_data_reviews)
-
 
 
 def last_page(url):
@@ -68,25 +92,27 @@ soup2 = soup(soup1.prettify(),'html.parser')
 list_hotels = soup2.find('div', {'class':'relWrap'})
 hotels = list_hotels.findAll('div', {'class':'prw_rup prw_meta_hsx_responsive_listing ui_section listItem'})
 
+
+
 for hotel in hotels:
 
-    link = hotel.find('a', {'class': 'property_title prominent'}, href=True)
-    if (link):
+    if ( not full ):
 
-        url_origin = "https://www.tripadvisor.com" + link['href']
-        url = url_origin
-        scrap_data(url)
-        cmp = 0
+        link = hotel.find('a', {'class': 'property_title prominent'}, href=True)
+        if (link):
 
-        while (not last_page(url)):
-
-            cmp += number_reviews(url)
-            url = re.sub(r'(Reviews-)', "or" + str(cmp) + "-", url_origin)
+            url_origin = "https://www.tripadvisor.com" + link['href']
+            url = url_origin
             scrap_data(url)
+            cmp = 0
+            while (not last_page(url) and not full ):
 
-            
+                cmp += number_reviews(url)
+                url = re.sub(r'(Reviews-)', "or" + str(cmp) + "-", url_origin)
+                scrap_data(url)
 
-
+    else:
+        break
 
 
 
