a7ccbd3eb6c9ca5966804291e19bc519db77ce6a
hyungkawmld <moulahidkawtar@email.com>
2022-03-21 23:11:57 +0100

    Add final updates

--- reviews_classifier.py
+++ reviews_classifier.py
@@ -1,47 +1,24 @@
+
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import f1_score
-from nettoyage import *
-
-def nettoyerDataframe(df):
-    df["text"] = df.Review.map(lambda x: remove_URL(x))
-    df["text"] = df.Review.map(lambda x: remove_html(x))
-    df["text"] = df.Review.map(lambda x: remove_emoji(x))
-    df["text"] = df.Review.map(lambda x: remove_punct(x)) 
-    df["text"] = df.Review.map(remove_stopwords)   
-    return df
-
-
-def tfidf(data, ngrams=(1, 1)):
-    # use_idf=False
-    #sublinear_tf=True
-    tfidf_vectorizer = TfidfVectorizer(ngram_range=ngrams)
-    train = tfidf_vectorizer.fit_transform(data)
-    return train, tfidf_vectorizer
 
 
 df = pd.read_csv('tripadvisor_hotel_reviews.csv')
 
-df=nettoyerDataframe(df)
+X_train, X_test, y_train, y_test = train_test_split(df['Review'], df['Rating'], test_size=0.2)
 
+vect = TfidfVectorizer().fit(X_train)
 
-train_tfidf, xxx = tfidf(df["Review"])
-
-X = train_tfidf.todense()[0:][0:].tolist()
-
-X_train, X_test, y_train, y_test = train_test_split(X, df['Rating'], test_size=0.2)
-
-#vect = TfidfVectorizer().fit(X_train)
-
-#X_train_vectorised = vect.transform(X_train)
+X_train_vectorised = vect.transform(X_train)
 
 
 model = LogisticRegression(multi_class='multinomial')
-model.fit(X_train, y_train)
-
-y_pred = model.predict(X_test)
+model.fit(X_train_vectorised, y_train)
+xxx
+y_pred = model.predict(vect.transform(X_test))
 
 print(y_pred)
 
